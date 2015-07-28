11299147b464fa6b2edd99a59ee1c6ca4e7d4227
Adriaan <adriaan.larmuseau@gmail.com>
2015-07-28 23:13:16 +0300

    portfolio earnings

---
+++ portfolio/portfolio.py
@@ -0,0 +1,70 @@
+from __future__ import absolute_import
+
+import json
+import re
+from portfolio.extract import is_currency, get_quote
+from portfolio.holding import Holding
+
+#======================================
+# Exceptions
+#======================================
+
+class IncorrectConfig(Exception):
+
+  def __init__(self,note):
+    self.note = note
+
+    def __str__(self):
+      return "Failed to parse config: " + self.note
+
+
+#==== convert to new currency =====
+def to_curr(amount,old,new):
+  if old == new :
+    return amount
+  quote = get_quote(old + new + ':CUR')
+  return (quote.price * float(amount))
+      
+
+
+#== Class =============================
+#   * Porfolio
+#   -- A collection of holdings 
+#   -- That provides a certain earning
+#======================================
+class Porfolio(object):
+
+  def __init__(self,config):
+    decode = None 
+    try:
+      decode = json.loads(config)
+    except: 
+      raise IncorrectConfig("Flawed json")
+    if not decode.has_key('currency'):
+      raise IncorrectConfig('Currency not defined') 
+    elif not is_currency(decode['currency']):
+      raise IncorrectConfig('Unknown currency')
+    elif not (decode.has_key('holdings') and len(decode['holdings']) > 0):
+      raise IncorrectConfig('No holdings defined') 
+    m = re.match("(\w+)(:.*|$)", decode['currency'])
+    self.currency = m.groups()[0]
+    self.holdings = []
+    for holding in decode['holdings']:
+      self.holdings.append(Holding(holding))
+    gains = map(lambda x: to_curr(x.change,x.currency,self.currency),self.holdings)
+    self.gain = reduce((lambda x,y: x + y),gains)
+
+  def earnings_statement(self):
+    return ("Total earnings: " + str(self.gain) + " (" + self.currency + ')')
+
+  def __str__(self):
+    str_list = []
+    for holding in self.holdings:
+      str_list.append(str(holding))
+    str_list.append(self.earnings_statement())
+    out_str = "\n".join(str_list)
+    return out_str
+
+      
+    
+
